655ee2250384699e7b86268758447930ea2e448c
umanggoel0705 <129640795+umanggoel0705@users.noreply.github.com>
2024-06-27 11:44:11 +0530

    Add files via upload

--- script.py
+++ script.py
@@ -6,16 +6,26 @@ model = YOLO('yolov8n.pt')
 cap = cv2.VideoCapture(0)
 
 while cv2.waitKey(1) != 27:
-    # _, frame = cap.read()
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
 
-    # if not _:
-    #     break
+    if not _:
+        break
     
-    results = model.predict(source=0, show=True, stream=True, classes=[62, 63, 67])
+    results = model.predict(source=frame, stream=True, classes=[62, 63, 67])
     if results:
-        for result in enumerate(results):
-            x1, y1, x2, y2 = result.boxes.xyxy[0]
-            print(x1, y1, x2, y2)
+        for result in results:
+            boxes = result.boxes.cuda()
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1 = int(x1)
+                y1 = int(y1)
+                x2 = int(x2)
+                y2 = int(y2)
+                cv2.rectangle(frame, (x1,y1), (x2,y2), (0,0,0), 3)
+                roi = frame[y1:y2, x1:x2]
+                roi = cv2.blur(roi, (15, 15))
+                frame[y1:y2, x1:x2] = roi
     else:
         print("no object")
-    # cv2.imshow("Camera", frame)
+    cv2.imshow("Camera", frame)
